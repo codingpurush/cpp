5100f8a625d29ec894a3ed2ecdb8fe987c481429
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-03 12:17:17 +0530

    Create break_continue.cpp

---
+++ break_continue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i{1};
+    
+    while (i<=10){
+        cout << i << endl;
+        if (i==7)
+            break;
+        i=i+1;
+     }
+     return 0;
+     
+}
