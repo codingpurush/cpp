2c9e6e6feac69d7750ba45ab5d9bf825be3347c3
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-03 18:35:40 +0530

    Create Fibonacci_of_N_terms.cpp

---
+++ Fibonacci_of_N_terms.cpp
@@ -0,0 +1,31 @@
+///This code prints the fibonacci of N terms and also prints their sum.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ///Fibonacci series of N terms.
+
+    int n{0}, a{0}, b{1}, c=a+b , sum{0};
+    cin >> n;
+
+    cout <<a<<","<<b<<",";
+
+    int i{2};           /// If f0 = 0, f1 = 1, f2 = 1, f3 = 2, f4 = 3, f5 = 5 put 1, it will run one more time 
+    while(i<n){         /// as i keeps count of no. of terms
+        sum=sum+c;
+        cout << c << ",";
+        a=b;
+        b=c;
+        c=a+b;
+        i++;
+    }
+
+    cout << "\n \n";
+    cout << "Number of terms printed : " << i<<endl;
+    cout <<"The sum of this series is : "<< sum+1;
+    return 0;
+
+
+}
