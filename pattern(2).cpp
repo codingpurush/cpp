625672ce0ba087fa184ba7560bd7a414d248c39a
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-03 14:56:49 +0530

    Create pattern(2).cpp

---
+++ pattern(2).cpp
@@ -0,0 +1,37 @@
+/*
+    1
+   123
+  12345
+ 1234567
+123456789
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n{0}, i{1};
+    cin >> n;
+
+    while (i<=n)   ///i denotes row number.
+    {
+        ///spaces
+        int space = 1;
+        while (space<=n-i){
+            cout << " ";
+            space++;
+        }
+
+        ///numbers
+        int number = 1;
+        while (number<=2*i - 1){
+            cout << number<< "";
+            number++;
+        }
+
+        cout << endl;
+        i++;
+    }
+}
