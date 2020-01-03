0bae24a43991ed8e84d136f3343a56979e89eb44
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-03 19:07:40 +0530

    Update Fibonacci_of_N_terms.cpp

--- Fibonacci_of_N_terms.cpp
+++ Fibonacci_of_N_terms.cpp
@@ -10,10 +10,10 @@ int main()
     int n{0}, a{0}, b{1}, c=a+b , sum{0};
     cin >> n;
 
-    cout <<a<<","<<b<<",";
+    cout <<a<<" "<<b<<" ";
 
     int i{2};           /// If f0 = 0, f1 = 1, f2 = 1, f3 = 2, f4 = 3, f5 = 5 put 1, it will run one more time 
-    while(i<n){         /// as i keeps count of no. of terms
+    while(i<n){         /// as i keeps count of no. of terms, here a = 0 is taken as 1st term not 0th
         sum=sum+c;
         cout << c << " ";
         a=b;
@@ -22,9 +22,9 @@ int main()
         i++;
     }
 
-    cout << "\n \n";
+    cout << "\n";
     cout << "Number of terms printed : " << i<<endl;
-    cout <<"The sum of this series is : "<< sum+1;
+    cout <<"The sum of this series is : "<< sum+1<<endl;
     return 0;
 
 
