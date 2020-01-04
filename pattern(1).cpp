84198988678ea8b8abbcc9654369633292aedb15
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-04 12:12:40 +0530

    Update pattern(1).cpp

--- pattern(1).cpp
+++ pattern(1).cpp
@@ -18,6 +18,11 @@
 4444
 55555
 
+1
+23
+456
+78910
+
 */
 
 #include<iostream>
@@ -36,6 +41,33 @@ int main ()
         while (j<=i){
             cout << "*";
             j=j+1;
+        }
+        cout << endl;
+        i=i+1;
+    }
+    return 0;
+}
+
+/// For the fourth pattern another variable needs to be defined named counter (k), print k and increment it.
+/// Code:
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int n{0};
+    cout << "Enter number of lines: " << endl;
+    cin >> n;
+
+    int i{1},j;
+    int k{1};
+
+    while (i<=n){
+        j=1;          /// j is initialized every time before it is checked
+        while (j<=i){
+            cout << k;
+            j=j+1;
+            k=k+1;
         }
         cout << endl;
         i=i+1;
