45b37f73d49a08d06e523be238575cb20cb501f7
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-04 12:19:37 +0530

    Update pattern(1).cpp

--- pattern(1).cpp
+++ pattern(1).cpp
@@ -74,3 +74,28 @@ int main ()
     }
     return 0;
 }
+
+///Another approach for 4 pattern
+
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int n{0};
+    cout << "Enter number of lines: " << endl;
+    cin >> n;
+
+    int i{1},j;
+
+    while (i<=n){
+        j=1;          /// j is initialized every time before it is checked
+        while (j<=i){
+            cout << i*(i+1)/2+j;
+            j=j+1;
+        }
+        cout << endl;
+        i=i+1;
+    }
+    return 0;
+}
