9fc5018ff9af111d7fa602f1b93cd55403d01d86
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-05 08:33:27 +0530

    Create pattern(6).cpp

---
+++ pattern(6).cpp
@@ -0,0 +1,28 @@
+/*
+
+*****
+****
+***
+**
+*
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i=1; i<=n; i++) {
+        int j = 1;
+        for (int j=n; j>=i; j--){
+            cout << "*";
+        }
+        cout <<endl;
+    }
+    return 0;
+
+}
