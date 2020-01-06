56d243d71ae8d57baa3624149f14d97c340badca
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-06 23:45:57 +0530

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -4,11 +4,13 @@ using namespace std;
 
 int main()
 {
-    int n;
+    int n,result=0;
     cin>>n;
     int d=0;
     for (int i=n; i>0; i=i/10){
             d=i%10;
-            cout << d;
-          }
+            ///cout << d;            /// to just print reverse
+        result=result*10+d;          /// to store output
+       }
+    cout << result;
 }
