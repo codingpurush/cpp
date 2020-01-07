a99d28622c7fbb0ceb3773480acfe88fc989e50a
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-07 11:21:47 +0530

    Update rhombus_2.cpp

--- rhombus_2.cpp
+++ rhombus_2.cpp
@@ -1,33 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-
-    int n;
-    cin>>n;
-    int ijaisa;
-
-    for(int i=1; i<=2*n-1; i++)
-
-    {
-        if (i<=n)
-            ijaisa=i;
-        else
-            ijaisa=2*n-i;
-
-        /// Spaces
-        for(int j=1; j<=n-ijaisa; j++){
-            cout << "  ";
+int main(){
+    
+    int n; cin>>n;
+    
+    for (int i=1;i<=2*n-1;i=i+1){
+        int iJaisa;
+        if (i<=n) iJaisa=i; else iJaisa=2*n-i;
+        
+        for (int j=1;j<=n-iJaisa;j=j+1){
+            cout<<"  ";
         }
-
-        /// Stars
-        for(int k=1; k<=2*ijaisa-1; k++){
-            cout << "* ";
+        
+        for (int j=1;j<=2*iJaisa-1;j=j+1){
+            cout<<"* ";
         }
-
-        cout << endl;
+        
+        cout<<endl;
     }
-
 }
-
