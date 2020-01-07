1799524c13b264a6256d7040a1dd42843ca2ea33
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-07 11:32:41 +0530

    Update greater_of_N.cpp

--- greater_of_N.cpp
+++ greater_of_N.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
-
 using namespace std;
 
-int main()
-{
-    int n , max = INT_MIN;
-    cin >> n;
-
-    for (int i=1; i<=n; i++){
-            int a; cin>>a;
-            if (a>max) max=a;
-        }
-    cout << max;
-    return 0;
+int main(){
+    int n; cin>>n;
+    int Max=INT_MIN;
+    for (int i=1;i<=n;i=i+1){
+        int a; cin>>a;
+        if (a>Max) Max=a;
+    }
+    cout<<Max;
 }
+/*
+int main(){
+    int n; cin>>n;
+    int Max;
+    for (int i=1;i<=n;i=i+1){     /// If first number has to be assigned max value
+        int a; cin>>a;
+        if (i==1) Max=a;
+        else if (a>Max) Max=a;
+    }
+    cout<<Max;
+}*/
