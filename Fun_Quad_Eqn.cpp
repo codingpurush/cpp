75a6e54b1d97b2616e730dfe9222b943b2a6c9fe
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-01-12 10:37:14 +0530

    Update Fun_Quad_Eqn.cpp

--- Fun_Quad_Eqn.cpp
+++ Fun_Quad_Eqn.cpp
@@ -1,24 +1,31 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
-void quadeqn(int a, int b, int c)
+bool quadeqn(int a, int b, int c,float &r1, float &r2)
 {
      int d  = b*b - 4*a*c;
      double sqrt_val = sqrt(abs(d));
 
-     if (d > 0) cout << (double)(-b + sqrt_val)/(2*a) << endl << (double)(-b - sqrt_val)/(2*a);
+     if (d>=0)
+     {
+         r1=(double)(-b + sqrt_val)/(2*a);
+         r2=(double)(-b - sqrt_val)/(2*a);
+         return true;
+     }
 
-     else if (d == 0) cout << "same roots : " << -(double)b / (2*a);
-
-     else cout << "I dont know how to calculate this.";
+     else
+        cout << "I don't know how to calculate this.";
+        return false;
 }
 
 int main()
 {
-    int a,b,c;
+    float a,b,c,r1,r2;
     cout << "Enter 3 numbers: ";
     cin>>a>>b>>c;
     cout << endl;
-    quadeqn(a, b, c);
+    if (quadeqn(a, b, c ,r1 ,r2))
+
+    cout<<"Roots: "<<r1<<endl<<r2;
     return 0;
 }
