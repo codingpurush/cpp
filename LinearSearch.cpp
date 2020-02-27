d548d1042aee15a661fcc6cd914ed311a6760ccb
Chirag Bali <59139335+chiragbalicpp@users.noreply.github.com>
2020-02-27 08:46:56 -0800

    Update LinearSearch.cpp

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,45 +1,19 @@
-/// Code :
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
+int search(int arr[], int n, int x)
 {
-    int n,i,res=0; cin>>n;
-    int arr[]={1,5,3,6,4,8,9};
-    int x=sizeof(arr)/sizeof(arr[0]);
-
-    for (i=0;i<x;i++){
-          if(arr[i]==n){
-              res=1;
-              break;
-              }
-          }
-
-        if (res==0) cout << "not found"; else cout << "At index = " << i;
-
-    }
-
-/// Function :
-
-#include<iostream>
-using namespace std;
-
-int lsearch(int arr[],int x,int n)
-{
-    for(int i=0;i<x;i++){
-        if (arr[i]==n)
+    int i;
+    for (i = 0; i < n; i++)
+        if (arr[i] == x)
             return i;
-    }
     return -1;
 }
 
 int main()
 {
-    int n;cin>>n;
-    int arr[]={1,4,6,7,11,9};
-    int x=sizeof(arr)/sizeof(arr[0]);
-    cout << lsearch(arr,x,n);
+    int arr[]={1,2,3,4,5};
+    cout<<search(arr,5,4);
     return 0;
-}
 
+}
